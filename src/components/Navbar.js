4c282a683ac5ec45ec801423a977b84674560a96
ramzi-omari <ramziomari1995@gmail.com>
2020-12-27 21:38:49 +0100

    Navbar responsive done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/MenuData";
 import { Button } from "./Button";
+import { FaBars } from "react-icons/fa";
 
 const Nav = styled.nav`
   height: 60px;
@@ -31,12 +32,29 @@ const Logo = styled(Link)`
   font-weight: 600;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled(FaBars)`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-size: contain;
+    height: 40px;
+    width: 20px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 25%);
+  }
+`;
 
 const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavMenuLinks = styled(Link)`
@@ -47,6 +65,9 @@ const NavBtn = styled.div`
   display: flex;
   align-items: center;
   margin-right: 24px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const Navbar = () => {
