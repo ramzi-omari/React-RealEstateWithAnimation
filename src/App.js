f2b2eb05df700413412f1dd21b7f3ad8ddd61608
ramzi-omari <ramziomari1995@gmail.com>
2020-12-28 14:17:30 +0100

    Body, InfoSection Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
+import InfoSection from "./components/InfoSection";
 import Navbar from "./components/Navbar";
+import { InfoData } from "./data/InfoData";
 import { SliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyles";
 
@@ -17,6 +19,7 @@ function App() {
       <Navbar toggle={toggle}></Navbar>
       <Dropdown isOpen={isOpen} toggle={toggle}></Dropdown>
       <Hero slides={SliderData}></Hero>
+      <InfoSection {...InfoData}></InfoSection>
     </>
   );
 }
---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from "../images/house-5.jpg";
+import ImageTwo from "../images/house-6.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus dicta nemo cupiditate eveniet, mollitia inventore reiciendis accusantium quas cumque facilis.`,
+  paragraphTwo: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam animi expedita nesciunt, fugit autem.`,
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne: `Lorem ipsum dolor sit, amet  eveniet, mollitia inventore reiciendis accuconsectetur adipisicing elit. Temporibus dicta nemo cupiditatsantium quas cumque facilis.`,
+  paragraphTwo: `Lorem ipsum dolor sit amet consectetur nesciunt, fugit autem.`,
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
